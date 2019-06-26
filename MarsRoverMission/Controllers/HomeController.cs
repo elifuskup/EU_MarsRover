3785e27ad1fbd42f01d42b794a8395f4d4ac4ea8
elifuskup <40965886+elifu@users.noreply.github.com>
2019-06-26 09:50:03 +0300

    First Commit
    First commit

---
+++ MarsRoverMission/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using MarsRoverMission.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using static MarsRoverMission.Models.Rover;
+
+namespace MarsRoverMission.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Rover(string coordinates, string pos_r_1, string move_r_1)
+        {
+            var coords = coordinates.Split(' ');
+            var list_pos_r_1 = pos_r_1.Split(' ');
+
+            Rover rv = new Rover(Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1]));
+            rv.LandRover(Convert.ToInt16(list_pos_r_1[0]), Convert.ToInt16(list_pos_r_1[1]), (Directions)Enum.Parse(typeof(Directions), list_pos_r_1[2].ToString(), true));
+            rv.OperateRover(move_r_1);
+
+            return new PartialViewResult()
+            {
+                ViewName = "Rover",
+                ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+                {
+                    Model = rv,
+                }
+            };
+        }
+    }
+}
---
+++ MarsRoverMission/Models/Rover.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MarsRoverMission.Models
+{
+    public class Rover
+    {
+        /// <summary>
+        /// Assume that the square directly North from (x, y) is (x, y 1) ==> 
+        /// North will be equal to 1 and compass will be used as clockwise.
+        /// </summary>
+        private const int N = 1;
+        private const int E = 2;
+        private const int S = 3;
+        private const int W = 4;
+
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+        public int direction { get; set; }
+        public Directions directionName { get; set; }
+
+        public int MaxCoordinateX { get; set; }
+        public int MaxCoordinateY { get; set; }
+
+        public List<string> Moves;
+        public enum Directions
+        {
+            N = 1,
+            E = 2,
+            S = 3,
+            W = 4
+        }
+
+        public Rover(int x, int y)
+        {
+            MaxCoordinateX = x;
+            MaxCoordinateY = y;
+
+            positionX = 0;
+            positionY = 0;
+            direction = N;
+        }
+
+        /// <summary>
+        /// Function to set coordinates for landing
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="direction"></param>
+        public void LandRover(int x, int y, Directions direction)
+        {
+            this.positionX = x;
+            this.positionY = y;
+            this.direction = (int)direction;
+            this.Moves = new List<string>();
+            Moves.Add(positionX + "," + positionY);
+        }
+
+        /// <summary>
+        /// Function to move rover
+        /// Paint result list + Landing Coordinates
+        /// </summary>
+        /// <param name="movements"></param>
+        /// <returns></returns>
+        public string OperateRover(string movements)
+        {
+            bool paintIt = true;
+
+            foreach (var letter in movements.ToCharArray())
+            {
+                paintIt = true;
+                switch (letter)
+                {
+                    case 'L':
+                        GoTo_L();
+                        break;
+                    case 'R':
+                        GoTo_R();
+                        break;
+                    case 'M':
+                        GoTo_M();
+                        break;
+                    default:
+                        paintIt = false;
+                        break;
+                }
+                if (paintIt)
+                    Moves.Add(positionX + "," + positionY);
+            }
+
+            this.directionName = (Directions)direction;
+            return "All moved!";
+
+            // with Reflection
+            //var xMethod = this.GetType().GetMethod("GoTo_" + letter);
+            //if (xMethod != null)
+            //    xMethod.Invoke(this, new object[] { });
+            //else
+            //    throw new Exception("Wrong direction!");
+        }
+
+        /// <summary>
+        /// Function to MOVE FORWARD
+        /// </summary>
+        public void GoTo_M()
+        {
+            if (direction == (int)Directions.N)
+            {
+                this.positionY++;
+            }
+            else if (direction == (int)Directions.E)
+            {
+                this.positionX++;
+            }
+            else if (direction == (int)Directions.S)
+            {
+                this.positionY--;
+            }
+            else if (direction == (int)Directions.W)
+            {
+                this.positionX--;
+            }
+        }
+
+        /// <summary>
+        /// Function to TURN LEFT
+        /// </summary>
+        public void GoTo_L()
+        {
+            direction = (direction - 1) < N ? W : direction - 1;
+        }
+
+        /// <summary>
+        /// Function to TURN RIGHT
+        /// </summary>
+        public void GoTo_R()
+        {
+            direction = (direction + 1) > W ? N : direction + 1;
+        }
+    }
+}
